77cb5507b71da9206dfcdaaf97aebb7a53b2c978
Nando <I_will_set_later>
2021-06-05 13:45:31 -0400

    made changes to ch3 based on exercises in ch4

--- code/ch3-hello-fastapi-world/main.py
+++ code/ch3-hello-fastapi-world/main.py
@@ -1,15 +1,32 @@
 import fastapi
 import uvicorn
+import fastapi_chameleon
+from views import home, account, packages
 
 app = fastapi.FastAPI()
 
 
-@app.get('/')
-def index():
-    return {
-        'message': "Hello world"
-    }
+def main():
+    configure()
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
-if __name__ == '__main__':
-    uvicorn.run(app)
+def configure():
+    configure_templates()
+    configure_routes()
+
+
+def configure_templates():
+    fastapi_chameleon.global_init("templates")
+
+
+def configure_routes():
+    app.include_router(home.router)
+    app.include_router(account.router)
+    app.include_router(packages.router)
+
+
+if __name__ == "__main__":
+    main()
+else:
+    configure()
---
+++ code/ch3-hello-fastapi-world/views/account.py
@@ -0,0 +1,19 @@
+import fastapi
+from fastapi_chameleon import template
+
+router = fastapi.APIRouter()
+
+
+@router.get("/account")
+def index():
+    return {}
+
+
+@router.get("/account/register")
+def register():
+    return {}
+
+
+@router.get("/account/login")
+def login():
+    return {}
